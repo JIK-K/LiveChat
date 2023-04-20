ed0915a5a7103d78affafb6a795b6d77428afd75
JIK-K <87060676+JIK-K@users.noreply.github.com>
2023-04-20 18:00:54 +0900

    Add websocket, index.html

---
+++ LiveChat/src/main/java/com/LiveChat/LiveChat/Controller/MessageController.java
@@ -0,0 +1,48 @@
+package com.LiveChat.LiveChat.Controller;
+
+import jakarta.websocket.*;
+import jakarta.websocket.server.ServerEndpoint;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@ServerEndpoint("/websocket")
+public class MessageController extends Socket {
+    private static final List<Session> session = new ArrayList<Session>();
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
+    @OnOpen
+    public void open(Session newUser) {
+        System.out.println("connected");
+        session.add(newUser);
+        System.out.println(newUser.getId());
+    }
+
+    @OnMessage
+    public void getMsg(Session recieveSession, String msg) {
+        for (int i = 0; i < session.size(); i++) {
+            if (!recieveSession.getId().equals(session.get(i).getId())) {
+                try {
+                    session.get(i).getBasicRemote().sendText("상대 : "+msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                try {
+                    session.get(i).getBasicRemote().sendText("나 : "+msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- LiveChat/src/main/java/com/LiveChat/LiveChat/LiveChatApplication.java
+++ LiveChat/src/main/java/com/LiveChat/LiveChat/LiveChatApplication.java
@@ -2,6 +2,8 @@ package com.LiveChat.LiveChat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class LiveChatApplication {
@@ -10,4 +12,8 @@ public class LiveChatApplication {
 		SpringApplication.run(LiveChatApplication.class, args);
 	}
 
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
 }
