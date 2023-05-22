e19785aff04a28c04ae4f050fe60963a538366a9
JIK-K <87060676+JIK-K@users.noreply.github.com>
2023-05-22 20:46:21 +0900

    Add css, Client Class

--- LiveChat/src/main/java/com/LiveChat/LiveChat/Controller/MessageController.java
+++ LiveChat/src/main/java/com/LiveChat/LiveChat/Controller/MessageController.java
@@ -2,18 +2,24 @@ package com.LiveChat.LiveChat.Controller;
 
 import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import com.LiveChat.LiveChat.VO.Client;
 
 @Controller
 @ServerEndpoint("/websocket")
 public class MessageController extends Socket {
     private static final List<Session> session = new ArrayList<Session>();
+    private static final List<Client> ClientList = new ArrayList<>();
 
     @GetMapping("/")
     public String index() {
@@ -21,10 +27,17 @@ public class MessageController extends Socket {
     }
 
     @OnOpen
-    public void open(Session newUser) {
+    public void open(Session newClient) {
         System.out.println("connected");
-        session.add(newUser);
-        System.out.println(newUser.getId());
+        session.add(newClient);
+        System.out.println(newClient.getId());
+    }
+    
+    @OnClose
+    public void close(Session Client){
+        System.out.println("disconnected");
+        session.remove(Client);
+        System.out.println(Client.getId());
     }
 
     @OnMessage
@@ -45,4 +58,46 @@ public class MessageController extends Socket {
             }
         }
     }
+
+    // @OnMessage
+    // public void processMessage(Session ClientSession, String message) {
+    //     // Find the corresponding Client
+    //     Client currentClient = findClientBySession(ClientSession);
+
+    //     if (currentClient == null) {
+    //         // Create a new Client with the received message as the Clientname
+    //         Client newClient = new Client(message);
+    //         newClient.setSession(ClientSession);
+
+    //         // Add the Client to the Client list
+    //         ClientList.add(newClient);
+    //     } else {
+    //         // Process the message
+    //         String formattedMessage = currentClient.getName() + ": " + message;
+
+    //         // Send the message to other Clients
+    //         sendToOtherClients(ClientSession, formattedMessage);
+    //     }
+    // }
+
+    // private Client findClientBySession(Session ClientSession) {
+    //     for (Client Client : ClientList) {
+    //         if (Client.getSession().equals(ClientSession)) {
+    //             return Client;
+    //         }
+    //     }
+    //     return null;
+    // }
+
+    // private void sendToOtherClients(Session senderSession, String message) {
+    //     for (Session session : session) {
+    //         if (!session.equals(senderSession)) {
+    //             try {
+    //                 session.getBasicRemote().sendText(message);
+    //             } catch (IOException e) {
+    //                 e.printStackTrace();
+    //             }
+    //         }
+    //     }
+    // }
 }
---
+++ LiveChat/src/main/java/com/LiveChat/LiveChat/VO/Client.java
@@ -0,0 +1,28 @@
+package com.LiveChat.LiveChat.VO;
+
+import jakarta.websocket.Session;
+
+public class Client {
+    private String name;
+    private Session session;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+}
